f0b3c0ed67b45555ec53b152b6da808a31ca4cd3
Your Name <you@example.com>
2019-09-13 08:45:19 -0700

    done migrations

--- migrations/20190913081405_create-tables.js
+++ migrations/20190913081405_create-tables.js
@@ -1,8 +1,59 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('project', tbl => {
+        tbl.increments();
+        tbl.string('project_name',128).notNullable();
+        tbl.string('description');
+        tbl.boolean('completed').notNullable().defaultTo(false);
+
+    })
+    .createTable('resource', tbl => {
+        tbl.increments();
+        tbl.string('resource_name').notNullable().unique();
+        tbl.string('description');
+    })
+    .createTable('task', tbl => {
+        tbl.increments();
+        tbl.string('description').notNullable();
+        tbl.string('notes')
+        tbl.boolean('completed').notNullable().defaultTo(false);
+
+        // Foreign key
+        tbl
+            .integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('project')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+    })
+    // Foreign key
+    .createTable('project_resource', tbl => {
+        tbl
+            .integer('project_id')
+            .unsigned()
+            .references('id')
+            .inTable('project')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        tbl
+            .integer('resource_id')
+            .unsigned()
+            .references('id')
+            .inTable('resource')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE');
+        tbl.primary(['project_id', 'resource_id'])
+    })
+
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('project_resource')
+    .dropTableIfExists('task')
+    .dropTableIfExists('resource')
+    .dropTableIfExists('project')
+
 };
