432393e1b46f80c62d1c29923613a9bbb85450b8
Your Name <you@example.com>
2019-09-13 10:03:34 -0700

    fixed the error, seeded successfully and about to build API

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,9 @@
-// Update with your config settings.
-
 module.exports = {
-
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true, 
     connection: {
-      filename: './projects.db3'
+      filename: './project.db3',
     },
-    useNullAsDefault: true,
-    pool: {
-      afterCreate: (conn, done) => {
-        // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-      },
-    }
   },
-
-  
-
 };
